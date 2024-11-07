a652ddee9d0db7eea91959ef5fec9b122647fcf5
Lani23 <linalmg23@gmail.com>
2024-11-06 19:13:52 -0500

    Actividad clase 7

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+//import { useState } from "react";
+//mport reactLogo from "./assets/react.svg";
+//import viteLogo from "/vite.svg";
+import "./App.css";
+import Card from "./Components/Card";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Carga de estudiantes</h1>
+      <Card />
+    </div>
+  );
+}
+
+export default App;
