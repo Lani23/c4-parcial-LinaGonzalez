a26f819dbf9e2b17606df51de24c3ed7295a7706
Lani23 <linalmg23@gmail.com>
2024-11-06 22:37:45 -0500

    Parcial Front End

--- src/Components/Pinguino.jsx
+++ src/Components/Pinguino.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 
 const Pinguino = ({ pinguino, setIsSelected }) => {
-  //   console.log(espectaculo);
-
   return (
     <div className="pinguino-card">
       <div className="pinguino-content">
